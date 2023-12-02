0d6174895c3b73138b328d5d5a39249c59ac58dc
Manuth VANN <128027201+manuthvann216@users.noreply.github.com>
2023-12-03 01:07:55 +0700

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,38 +1,62 @@
 from fastapi import FastAPI, Form
-import cv2
 import numpy as np
-import pandas as pd
-import json
-import easyocr
+import pytesseract
+import cv2
+from io import BytesIO
 import requests
 
 app = FastAPI()
 
-# Define the EasyOCR Reader
-reader = easyocr.Reader(['en'])
+def format_table_data(text):
+    lines = text.strip().split('\n')
 
-def read_image_from_url(url):
-    response = requests.get(url)
-    img_array = np.asarray(bytearray(response.content), dtype=np.uint8)
-    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    return img
+    num_columns = 12
+    num_rows = len(lines) // num_columns
 
-def perform_ocr(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+   
+    tabular_data = []
+
     
-    # Use EasyOCR to extract text
-    results = reader.readtext(gray)
+    for i in range(num_rows):
+        # Extract columns for the current row
+        row_columns = lines[i * num_columns: (i + 1) * num_columns]
 
-    # Extracting text without coordinates
-    text_data = [result[1] for result in results]
+        # Create a list to store the columns for the current row
+        row_data = []
 
-    # Convert list to JSON
-    json_data = json.dumps(text_data)
+        # Iterate over columns
+        for column in row_columns:
+            # Split each column into individual values
+            column_values = column.strip().split()
 
-    return json_data
+            # If there are multiple values in a column, keep them together
+            if len(column_values) > 1:
+                row_data.extend([' '.join(column_values)])
+            else:
+                row_data.extend(column_values)
+
+        tabular_data.append(row_data)
+
+    return {"data": [tabular_data]}
+
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the OCR API"}
 
 @app.post("/ocr-from-url/")
 async def perform_ocr_from_url(url: str = Form(...)):
-    image = read_image_from_url(url)
-    text = perform_ocr(image)
-    return {"text": text}
+    response = requests.get(url)
+    img_array = np.asarray(bytearray(response.content), dtype=np.uint8)
+    image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    text = pytesseract.image_to_string(thresh)
+
+    # Format the table data
+    tabular_data = format_table_data(text)
+
+    return {"data": tabular_data}
