1d12cf6f97b0bfad9d6101b06412c8901801e443
Manuth VANN <128027201+manuthvann216@users.noreply.github.com>
2023-12-04 07:21:53 +0700

    half way done :(

--- src/main.py
+++ src/main.py
@@ -2,102 +2,111 @@ from fastapi import FastAPI, Form
 import numpy as np
 import pytesseract
 import cv2
-from io import BytesIO
 import requests
-
+import uvicorn
+import json 
 app = FastAPI()
 
-def extract_tabular_data(img):
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, img_bin_otsu = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+def format_table_data(text):
+    lines = text.strip().split('\n')
 
-    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, np.array(img).shape[1] // 100))
-    eroded_image = cv2.erode(img_bin_otsu, vertical_kernel, iterations=3)
-    vertical_lines = cv2.dilate(eroded_image, vertical_kernel, iterations=3)
-    hor_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (np.array(img).shape[1] // 100, 1))
-    horizontal_lines = cv2.erode(img_bin_otsu, hor_kernel, iterations=5)
-    horizontal_lines = cv2.dilate(horizontal_lines, hor_kernel, iterations=5)
-    vertical_horizontal_lines = cv2.addWeighted(vertical_lines, 0.5, horizontal_lines, 0.5, 0.0)
-    vertical_horizontal_lines = cv2.erode(~vertical_horizontal_lines, vertical_kernel, iterations=3)
-    _, vertical_horizontal_lines = cv2.threshold(vertical_horizontal_lines, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    bitxor = cv2.bitwise_xor(img, vertical_horizontal_lines)
-    bitnot = cv2.bitwise_not(bitxor)
+    num_columns = 12
+    num_rows = len(lines) // num_columns
 
-    contours, hierarchy = cv2.findContours(vertical_horizontal_lines, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    boundingBoxes = [cv2.boundingRect(contour) for contour in contours]
-    (contours, boundingBoxes) = zip(*sorted(zip(contours, boundingBoxes), key=lambda x: x[1][1]))
+    tabular_data = []
 
-    boxes = []
-    for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
-        if (w < 1000 and h < 500):
-            boxes.append([x, y, w, h])
+    for i in range(num_rows):
+        # Extract columns for the current row
+        row_columns = lines[i * num_columns: (i + 1) * num_columns]
 
-    rows = []
-    columns = []
-    heights = [boxes[i][3] for i in range(len(boxes))]
-    mean = np.mean(heights)
-    columns.append(boxes[0])
-    previous = boxes[0]
+        # Create a list to store the columns for the current row
+        row_data = []
 
-    for i in range(1, len(boxes)):
-        if boxes[i][1] <= previous[1] + mean / 2:
-            columns.append(boxes[i])
-            previous = boxes[i]
-            if i == len(boxes) - 1:
-                rows.append(columns)
-        else:
-            rows.append(columns)
-            columns = []
-            previous = boxes[i]
-            columns.append(boxes[i])
+        # Iterate over columns
+        for column in row_columns:
+            # Split each column into individual values
+            column_values = column.strip().split()
 
-    total_cells = 0
-    for i in range(len(rows)):
-        if len(rows[i]) > total_cells:
-            total_cells = len(rows[i])
-
-    center = [int(rows[i][j][0] + rows[i][j][2] / 2) for j in range(len(rows[i])) if rows[0]]
-    center = np.array(center)
-    center.sort()
-
-    boxes_list = []
-    for i in range(len(rows)):
-        l = []
-        for k in range(total_cells):
-            l.append([])
-        for j in range(len(rows[i])):
-            diff = abs(center - (rows[i][j][0] + rows[i][j][2] / 4))
-            minimum = min(diff)
-            indexing = list(diff).index(minimum)
-            l[indexing].append(rows[i][j])
-        boxes_list.append(l)
-
-    dataframe_final = []
-    for i in range(len(boxes_list)):
-        for j in range(len(boxes_list[i])):
-            s = ''
-            if len(boxes_list[i][j]) == 0:
-                dataframe_final.append(' ')
+            # If there are multiple values in a column, keep them together
+            if len(column_values) > 1:
+                row_data.extend([' '.join(column_values)])
             else:
-                for k in range(len(boxes_list[i][j])):
-                    y, x, w, h = boxes_list[i][j][k][0], boxes_list[i][j][k][1], boxes_list[i][j][k][2], boxes_list[i][j][k][3]
-                    roi = bitnot[x:x + h, y:y + w]
-                    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 1))
-                    border = cv2.copyMakeBorder(roi, 2, 2, 2, 2, cv2.BORDER_CONSTANT, value=[255, 255])
-                    resizing = cv2.resize(border, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-                    dilation = cv2.dilate(resizing, kernel, iterations=1)
-                    erosion = cv2.erode(dilation, kernel, iterations=2)
-                    out = pytesseract.image_to_string(erosion)
-                    if len(out) == 0:
-                        out = pytesseract.image_to_string(erosion)
-                    s = s + " " + out
-                dataframe_final.append(s)
+                row_data.extend(column_values)
 
-    arr = np.array(dataframe_final)
-    import pandas as pd
-    dataframe = pd.DataFrame(arr.reshape(len(rows), total_cells))
-    return {"data": [dataframe.values.transpose().tolist()]}
+        tabular_data.append(row_data)
+
+    return tabular_data
+
+def organize_data(data):
+    organized_data = {"data": [[]]}
+    current_row = 0
+
+    for i, value in enumerate(data):
+        if i % 7 == 0 and i != 0:
+            organized_data["data"].append([])
+            current_row += 1
+
+        organized_data["data"][current_row].append(value)
+
+    return organized_data
+
+def convert_to_specific_format(organized_data):
+    formatted_data = {"data": [[]]}
+    
+    for row in organized_data["data"]:
+        formatted_row = []
+        for i in range(len(row[0])):
+            formatted_cell = [
+                row[j][i] if i < len(row[j]) else "" for j in range(len(row))
+            ]
+            formatted_row.append(formatted_cell)
+
+        formatted_data["data"].append(formatted_row)
+
+    return formatted_data
+
+def additional_step(specific_text):
+    transformed_data = {"data": [[]]}
+
+    for row in specific_text["data"][1:]:
+        transformed_row = []
+        for item in row:
+            new_item = [
+                item[0].replace(" ", ""),
+                item[2],
+                item[4],
+                item[5],
+                item[1].replace("M", ""),
+                item[3],
+                item[6],
+                item[1].replace("M", "") if item[1] != "24-36 M" else "",
+            ]
+            transformed_row.append(new_item)
+
+        transformed_data["data"][0].extend(transformed_row)
+
+    return transformed_data
+
+def final_step(half_way_done_data):
+
+    formatted_data = []
+
+    for sublist in half_way_done_data["data"][0]:
+        formatted_sublist = [
+            sublist[0].replace("M", " M").replace(" ", ""),  # Format the first element
+            sublist[1],
+            sublist[2],
+            sublist[5],
+            sublist[4].replace(" ", "") + " " + sublist[1],
+            sublist[7] if sublist[7] != "" else "0.00%",  # Handle the case where the last element is an empty string
+            sublist[4].replace(" ", "") + " " + sublist[2],
+            sublist[4].replace(" ", "") + " " + sublist[6]
+        ]
+        formatted_data.append(formatted_sublist)
+
+    output_data = {"data": [formatted_data]}
+
+    return output_data
 
 @app.get("/")
 def read_root():
@@ -109,7 +118,24 @@ async def perform_ocr_from_url(url: str = Form(...)):
     img_array = np.asarray(bytearray(response.content), dtype=np.uint8)
     image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
-    # Extract tabular data
-    tabular_data = extract_tabular_data(image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    text = pytesseract.image_to_string(thresh)
 
-    return tabular_data
+
+    # Format the table data
+    tabular_data = format_table_data(text)
+
+    # Organize the data into the desired structure
+    organized_data = organize_data(tabular_data)
+
+    # Convert to the specific format
+    formatted_data = convert_to_specific_format(organized_data)
+
+    #additional  step 
+    # half_way_done_result = additional_step(formatted_data) 
+
+    #final step
+    # result = final_step(half_way_done_result)
+
+    return formatted_data
